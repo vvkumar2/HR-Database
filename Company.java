6624e7f26544e00369a394ab729c934afc01feec
Varun Kumar <vvkumar5623@gmail.com>
2021-12-30 23:26:14 -0500

    upload

---
+++ Company.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class Company here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class Company
+{
+    private ArrayList<Employee> employees;
+    private String companyName;
+    private String companyPassword;
+    public Company ()
+    {
+        employees = null;
+        companyName = null;
+        companyPassword = null;
+    }
+    public Company (ArrayList<Employee> a, String  c, String  p)
+    {
+        employees = a;
+        companyName = c;
+        companyPassword = p;
+    }
+    public ArrayList<Employee> getList()
+    {
+        return this.employees;
+    }
+    public String getName()
+    {
+        return this.companyName;
+    }
+    public Employee getEmployee(int x)
+    {
+        return this.employees.get(x);
+    }
+    public String getPassword()
+    {
+        return this.companyPassword;
+    }
+    public void setList(ArrayList<Employee> a)
+    {
+        this.employees = a;
+    }
+}
